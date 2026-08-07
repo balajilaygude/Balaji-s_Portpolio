cefa23e31270e185e473619e17447337c9508240
Balaji Laygude <balajiwork05@gmail.com>
2026-08-07 19:22:11 +0530

    Project section clean

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,10 +4,11 @@ import ai from "../assets/image1.png";
 import auth from "../assets/auth.png";
 import { Link } from "lucide-react";
 import { GitGraph } from "lucide-react";
+import { FaGithub  } from "react-icons/fa";
 
 export default function Projects() {
   return (
-    <section id="projects" className="px-6 md:px-20 py-16">
+    <section id="projects" className="min-h-[calc(100vh-100px)] px-6 md:px-20 py-16">
       <h2 className="text-3xl font-bold mb-10">Projects</h2>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3  gap-8">
@@ -15,15 +16,12 @@ export default function Projects() {
           <img
             src={auth}
             alt=""
-            className="h-full w-full object-cover rounded-md"
+            className="h-56 object-cover rounded-md"
           />
           <h3 className="font-semibold">Auth System</h3>
           <div className="flex flex-wrap gap-2 justify-center mt-3">
             <span className="text-xs bg-purple-100 text-purple-700 px-3 py-1 rounded-full">
               React
-            </span>
-            <span className="text-xs bg-blue-100 text-blue-700 px-3 py-1 rounded-full">
-              Tailwind
             </span>
             <span className="text-xs bg-gray-100 text-gray-700 px-3 py-1 rounded-full">
               Node
@@ -35,12 +33,6 @@ export default function Projects() {
               MongoDb
             </span>
           </div>
-          <p className="text-sm text-gray-600 mt-2">
-            Built a secure authentication system with user registration, login,
-            JWT-based authentication. Node.js,
-            Express.js, and MongoDB ,
-            React.js, Tailwind CSS, and React Router
-          </p>
           <div className="w-full flex justify-between items-center pt-3">
             <a
               href="https://auth-frontend-five-theta.vercel.app/"
@@ -57,7 +49,7 @@ export default function Projects() {
               rel="noopener noreferrer"
               className="flex justify-center items-center gap-2 text-blue-600 underline"
             >
-              <GitGraph className=" text" />
+              <FaGithub  className="w-8 h-8 "/>
               repo
             </a>
           </div>
@@ -66,7 +58,7 @@ export default function Projects() {
           <img
             src={ipl}
             alt=""
-            className="h-full w-full object-cover rounded-md"
+            className="h-56 object-cover rounded-md"
           />
           <h3 className="font-semibold">IPL Dashboard </h3>
           <div className="flex flex-wrap gap-2 justify-center mt-3">
@@ -80,11 +72,6 @@ export default function Projects() {
               Vercel
             </span>
           </div>
-          <p className="text-sm text-gray-600 mt-2">
-            Responsive IPL dashboard built with React and Tailwind CSS. Focused
-            on reusable components and clean UI. Deployed on Vercel for
-            production access.
-          </p>
           <div className="w-full flex justify-between items-center pt-3">
             <a
               href="https://ipl-project-react-psi.vercel.app/"
@@ -101,7 +88,7 @@ export default function Projects() {
               rel="noopener noreferrer"
               className="flex justify-center items-center gap-2 text-blue-600 underline"
             >
-              <GitGraph className=" text" />
+              <FaGithub  className="w-8 h-8 "/>
               repo
             </a>
           </div>
@@ -110,7 +97,7 @@ export default function Projects() {
           <img
             src={dice}
             alt=""
-            className="h-full w-full object-cover rounded-md"
+            className="h-56 object-cover rounded-md"
           />
           <h3 className="font-semibold">Dice Game </h3>
           <div className="flex flex-wrap gap-2 justify-center mt-3">
@@ -124,10 +111,7 @@ export default function Projects() {
               Vercel
             </span>
           </div>
-          <p className="text-sm text-gray-600 mt-2">
-            Created responsive dice game with dynamic state updates. smooth
-            gameplay logic. Hosted the project live on Vercel for easy access.
-          </p>
+
           <div className="w-full flex justify-between items-center pt-3">
             <a
               href="https://dics-game-react.vercel.app/"
@@ -144,7 +128,7 @@ export default function Projects() {
               rel="noopener noreferrer"
               className="flex justify-center items-center gap-2 text-blue-600 underline"
             >
-              <GitGraph className=" text" />
+              <FaGithub  className="w-8 h-8 "/>
               repo
             </a>
           </div>
