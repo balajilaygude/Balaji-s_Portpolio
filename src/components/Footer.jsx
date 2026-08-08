9b200975522bf605f1f86fd7d433d81a44985b56
Balaji Laygude <balajiwork05@gmail.com>
2026-08-08 11:52:23 +0530

    Footer is Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import { FaGithub,FaLinkedin  } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="bg-black text-white text-center py-4 text-sm">
       <a
         href="#home"
-        className="hover:text-purple-400 transition-colors duration-300"
+        className="font-mono font-semibold"
       >
         © {new Date().getFullYear()} Balaji Laygude — Portfolio
       </a>
