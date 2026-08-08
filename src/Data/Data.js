aaae9bebd4eeedbacb6ae934aea223d67116d3d7
Balaji Laygude <balajiwork05@gmail.com>
2026-08-08 12:39:02 +0530

    Data added

---
+++ src/Data/Data.js
@@ -0,0 +1,92 @@
+import auth from "../assets/auth.jpg"
+import ipl from "../assets/ipl.jpg"
+import dice from "../assets/dice.jpg"
+import ai from "../assets/ai.jpg"
+import event from "../assets/event.jpg"
+
+
+
+export const data = [
+  {
+    name: "Eventora - Event Booking & Management System",
+    languages: [
+      "React",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Mongoose",
+      "JWT",
+      "Nodemailer",
+      "bcrypt",
+      "Context API",
+      "Tailwind CSS"
+    ],
+    liveLink: "https://eventora-app-f9qc.onrender.com/",
+    githubLink: "https://github.com/balajilaygude/Eventora",
+    imageName: event
+  },
+  {
+    name: "AI Virtual Assistant",
+    languages: [
+      "React",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Mongoose",
+      "Gemini API",
+      "JWT",
+      "Cloudinary",
+      "Multer",
+      "Context API",
+      "Tailwind CSS"
+    ],
+    liveLink: "https://vitualassistant.onrender.com/",
+    githubLink: "https://github.com/balajilaygude/VitualAssistant",
+    imageName: ai
+  },
+    {
+    name: "Auth System",
+    languages: [
+      "React",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "JWT",
+      "Joi",
+      "bcrypt",
+      "Tailwind CSS"
+    ],
+    liveLink: "https://auth-frontend-five-theta.vercel.app/",
+    githubLink: "https://github.com/balajilaygude/Auth",
+    imageName: auth
+  },
+  {
+    name: "IPL 2024 Dashboard",
+    languages: [
+      "React",
+      "JavaScript",
+      "REST API",
+      "Tailwind CSS",
+      "React Router",
+      "Axios",
+      "Responsive Design"
+    ],
+    liveLink: "https://ipl-project-react-psi.vercel.app",
+    githubLink: "https://github.com/balajilaygude/IPL-Project-React",
+    imageName: ipl
+  },
+  {
+    name: "Dice Game",
+    languages: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "DOM Manipulation",
+      "Event Handling",
+      "Responsive Design"
+    ],
+    liveLink: "https://dics-game-react.vercel.app",
+    githubLink: "https://github.com/balajilaygude/React-Game",
+    imageName: dice
+  }
+];
